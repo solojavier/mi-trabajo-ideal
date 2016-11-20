3fe5137f648e3f28084527b013dead845eb577f8
Octavio Razo <octavio.razo.verduzco@gmail.com>
2016-11-20 12:31:25 -0600

    Revert "new test 2016 - changes"

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -24,20 +24,21 @@ class AnswersController < ApplicationController
   private
 
   def answer_params
-    params.require(:answer).permit(:username, :departamento, :antiguedad, :escolaridad, :genero, :edad,
-                                    :company_attribute_1, :company_attribute_2, :company_attribute_3, 
-                                    :work_attribute_1,:work_attribute_2, :work_attribute_3, :work_attribute_4, :work_attribute_5,
-                                    :work_attribute_value_1, :work_attribute_value_2, :work_attribute_value_3, :work_attribute_value_4, :work_attribute_value_5,
-                                    :company_mejora_1, :company_mejora_2, :company_mejora_3, :company_mejora_4 , :company_mejora_5,
-                                    :company, 
-                                    :current_feeling, 
-                                    :responsible,
-                                    :values, 
-                                    :values_description,
-                                    :score_boss_value, :score_company_value, 
-                                    :rank_training_value, :rank_development_value, :rank_work_value, :rank_team_value, :rank_information_value, 
-                                    :rank_voice_value, :rank_voice_boss_value, :rank_know_value, :rank_comunication_value, :rank_security_value, 
-                                    :rank_security_2_value, :rank_expectations_value, :rank_expectations_2_value, :rank_culture_value, :rank_boss_value, 
-                                    :rank_boss_2_value, :rank_respect_value, :rank_happywork_value, :rank_happywork_2_value, :current_feeling_2_value)
+    params.require(:answer).permit(:username, :company_attribute_1,
+                                   :company_attribute_2, :company_attribute_3,
+                                   :company_attribute_4, :company_attribute_5,
+                                   :action_taken, :action_missing,
+                                   :action_result, :work_attribute_1,
+                                   :work_attribute_2, :work_attribute_3,
+                                   :work_attribute_4, :work_attribute_5,
+                                   :work_attribute_value_1,
+                                   :work_attribute_value_2,
+                                   :work_attribute_value_3,
+                                   :work_attribute_value_4,
+                                   :work_attribute_value_5,
+                                   :work_attribute_selected_index,
+                                   :commitment_action, :commitment_date,
+                                   :company, :current_feeling, :responsible,
+                                   :values, :values_description)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161118010639) do
+ActiveRecord::Schema.define(version: 20151018195732) do
 
-  # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "answers", force: :cascade do |t|
@@ -21,6 +20,10 @@ ActiveRecord::Schema.define(version: 20161118010639) do
     t.string   "company_attribute_1"
     t.string   "company_attribute_2"
     t.string   "company_attribute_3"
+    t.string   "company_attribute_4"
+    t.string   "company_attribute_5"
+    t.string   "action_taken"
+    t.string   "action_result"
     t.string   "work_attribute_1"
     t.string   "work_attribute_2"
     t.string   "work_attribute_3"
@@ -31,45 +34,17 @@ ActiveRecord::Schema.define(version: 20161118010639) do
     t.integer  "work_attribute_value_3"
     t.integer  "work_attribute_value_4"
     t.integer  "work_attribute_value_5"
+    t.integer  "work_attribute_selected_index"
+    t.string   "commitment_action"
+    t.date     "commitment_date"
     t.string   "company"
-    t.datetime "created_at",                null: false
-    t.datetime "updated_at",                null: false
+    t.datetime "created_at",                    null: false
+    t.datetime "updated_at",                    null: false
+    t.text     "action_missing"
     t.integer  "current_feeling"
     t.integer  "responsible"
     t.string   "values"
     t.string   "values_description"
-    t.string   "departamento"
-    t.string   "antiguedad"
-    t.string   "escolaridad"
-    t.string   "genero"
-    t.integer  "edad"
-    t.string   "company_mejora_1"
-    t.string   "company_mejora_2"
-    t.string   "company_mejora_3"
-    t.string   "company_mejora_4"
-    t.string   "company_mejora_5"
-    t.integer  "score_boss_value"
-    t.integer  "score_company_value"
-    t.integer  "rank_training_value"
-    t.integer  "rank_development_value"
-    t.integer  "rank_work_value"
-    t.integer  "rank_team_value"
-    t.integer  "rank_information_value"
-    t.integer  "rank_voice_value"
-    t.integer  "rank_voice_boss_value"
-    t.integer  "rank_know_value"
-    t.integer  "rank_comunication_value"
-    t.integer  "rank_security_value"
-    t.integer  "rank_security_2_value"
-    t.integer  "rank_expectations_value"
-    t.integer  "rank_expectations_2_value"
-    t.integer  "rank_culture_value"
-    t.integer  "rank_boss_value"
-    t.integer  "rank_boss_2_value"
-    t.integer  "rank_respect_value"
-    t.integer  "rank_happywork_value"
-    t.integer  "rank_happywork_2_value"
-    t.integer  "current_feeling_2_value"
   end
 
 end
